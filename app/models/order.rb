a47491acc6282e86f70e3d8e37a0bd9711bc0ae9
Tyler Huyser <tyler.huyser@gmail.com>
2021-01-03 04:00:20 -0500

    added new method to Orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
+  before_validation :setFilledStatus
+  
   belongs_to :medication
   belongs_to :patient
   belongs_to :doctor
+
+  def setFilledStatus
+    self.filled = false
+  end
 
 end
