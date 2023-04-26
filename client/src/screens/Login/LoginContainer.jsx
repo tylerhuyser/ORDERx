62aa9659905b7029a96c64e7fcc56fb139ca8908
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-26 14:10:42 -0400

    reverted some CSS edits with the OrderCard component and added the background property to the LoginContainer -- rather than the Body-Container within the Lahyout Component.

--- client/src/screens/Login/LoginContainer.jsx
+++ client/src/screens/Login/LoginContainer.jsx
@@ -5,6 +5,8 @@ import LoginRouter from '../Login/LoginRouter'
 import Login from "../Login/Login"
 import Register from "../Register/Register"
 
+import "./LoginContainer.css"
+
 export default function LoginContainer (props) {
   
   const { userCategory, setUserCategory } = props;
