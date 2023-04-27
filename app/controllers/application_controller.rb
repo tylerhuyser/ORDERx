0d882678c1401aa467b1ff1e1af435ec7cb20ba2
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-27 15:55:18 -0400

    resolved backend issue with verify -- application assumed all users were doctors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,13 @@ class ApplicationController < ActionController::API
 
   def authorize_request
       header = request.headers['Authorization']
+      @userCategory = request.headers['User-Category']
+      puts @userCategory
       header = header.split(' ').last if header
       begin
         @decoded = decode(header)
-        @current_user = determine_user_model(@decoded[:id])
+        # @current_user = determine_user_model(@decoded[:id])
+        @current_user = determine_user_model(@decoded[:id], @userCategory)
       rescue ActiveRecord::RecordNotFound => e
         render json: { errors: e.message }, status: :unauthorized
       rescue JWT::DecodeError => e
@@ -24,19 +27,47 @@ class ApplicationController < ActionController::API
       end
     end
 
-  def determine_user_model(props)
-    @decoded = props
-    if Doctor.exists?(@decoded)
-      @user = Doctor.find(@decoded)
-      puts 'here 1'
-    elsif Patient.exists?(@decoded)
+  def determine_user_model(decoded, userCategory)
+    @decoded = decoded
+    @userCategory = userCategory
+    if @userCategory === 'patient'
+      puts 'user is a patient'
       @user = Patient.find(@decoded)
-      puts 'here 2'
+    elsif @userCategory === 'doctor'
+      puts 'user is a doctor'
+      @user = Doctor.find(@decoded)
     else
-      puts 'no user'
+      puts 'user is neither a doctor or patient'
     end
     return @user
   end
+
+  # def determine_user_model(props)
+  #   @decoded = props
+  #   if Doctor.exists?(@decoded)
+  #     @user = Doctor.find(@decoded)
+  #     puts 'here 1'
+  #   elsif Patient.exists?(@decoded)
+  #     @user = Patient.find(@decoded)
+  #     puts 'here 2'
+  #   else
+  #     puts 'no user'
+  #   end
+  #   return @user
+  # end
+
+  # def determine_user_model(props)
+  #   @decoded = props
+  #   puts @decoded
+  #   if Doctor.exists?(@decoded)
+  #     @user = Patient.find(@decoded)
+  #     puts 'here 2'
+  #   else
+  #     @user = Doctor.find(@decoded)
+  #     puts 'here 1'
+  #   end
+  #   return @user
+  # end
 
 
 end
