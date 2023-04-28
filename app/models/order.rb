fb14644874b3df12787dfd7ad257399d72a29d01
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-28 18:42:57 -0400

    additional edit to Order model in backend to allow for Filled status to be edited for Orders.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,7 @@
 class Order < ApplicationRecord
-  before_validation :setFilledStatus
   
   belongs_to :medication
   belongs_to :patient
   belongs_to :doctor
-
-  def setFilledStatus
-    self.filled = false
-  end
 
 end
